f6f879e6c93b66d9635f5015f3427f7d9c1edefa
Яна <yanasplanet@gmail.com>
2024-08-25 18:34:46 +0500

    quicksort algorithm added

--- srcs/algorithm/push_swap.c
+++ srcs/algorithm/push_swap.c
@@ -6,14 +6,14 @@
 /*   By: ysirkich <ysirkich@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:28:01 by ysirkich          #+#    #+#             */
-/*   Updated: 2024/08/21 16:24:36 by ysirkich         ###   ########.fr       */
+/*   Updated: 2024/08/22 01:43:36 by ysirkich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 static void	three_sort(t_stack **stack_a);
 static void	small_sort(t_stack **a, t_stack **b, int size);
-static void	sort(t_stack **stack_a, t_stack **stack_b, int size);
+static void	radix_sort(t_stack **stack_a, t_stack **stack_b, int size);
 
 void	push_swap(t_stack **stack_a, t_stack **stack_b, int size)
 {
@@ -30,7 +30,7 @@ void	push_swap(t_stack **stack_a, t_stack **stack_b, int size)
 	else if (size == 4 || size == 5)
 		small_sort(stack_a, stack_b, size);
 	else
-		sort(stack_a, stack_b, size);
+		radix_sort(stack_a, stack_b, size);
 }
 
 static void	three_sort(t_stack **stack_a)
@@ -44,25 +44,20 @@ static void	three_sort(t_stack **stack_a)
 	first = *stack_a;
 	second = (*stack_a)->next;
 	third = (*stack_a)->next->next;
-	//1. first > second > third
 	if (first->value > second->value && second->value > third->value)
 	{
 		instructions(stack_a, NULL, "sa");
 		instructions(stack_a, NULL, "rra");
 	}
-	//2. first > third > second
 	else if (first->value > third->value && third->value > second->value)
 		instructions(stack_a, NULL, "ra");
-	//3. second > first > third
 	else if (second->value > first->value && first->value > third->value)
 		instructions(stack_a, NULL, "sa");
-	//4. second > third > first
 	else if (second->value > third->value && third->value > first->value)
 	{
 		instructions(stack_a, NULL, "sa");
 		instructions(stack_a, NULL, "ra");
 	}
-	//5. third > first > second
 	else if (third->value > first->value && first->value > second->value)
 		instructions(stack_a, NULL, "rra");
 }
@@ -79,10 +74,14 @@ static void	small_sort(t_stack **a, t_stack **b, int size)
 	instructions(a, b, "pa");
 }
 
-static void	sort(t_stack **stack_a, t_stack **stack_b, int size)
+static void	radix_sort(t_stack **stack_a, t_stack **stack_b, int size)
 {
 	int	maximum_bits;
 	int	maximum_number;
 	int	index1;
 	int	index2;
+
+	maximum_bits = 0;
+	maximum_number = size - 1;
+	//the maximum number of bits needed
 }
---
+++ srcs/algorithm/quicksort.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quicksort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysirkich <ysirkich@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 16:57:40 by ysirkich          #+#    #+#             */
+/*   Updated: 2024/08/25 18:33:59 by ysirkich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int		partition(int *array, int low, int high);
+void	swap(int *a,int *b);
+
+void	quicksort(int *array, int low, int high)
+{
+	int	pivot;
+
+	if (high > low)
+	{
+		pivot = partition(array, low, high);//returns the index of the pivot after partitioning
+		quicksort(array, low, pivot - 1);
+		quicksort(array, pivot + 1, high);
+	}
+}
+
+int	partition(int *array, int low, int high)
+{
+	int	pivot;
+	int	index; //the next smaller element
+	int	current_element;
+
+	pivot = array[high];
+	index = low - 1;
+	current_element = low;
+	while (current_element < high)
+	{
+		if (array[current_element] <= pivot)
+		{
+			index++;
+			swap(array[index], array[current_element]);
+		}
+		current_element++;
+	}
+	swap(&array[index + 1], &array[high]);//pivot to the correct position
+	return (index + 1);
+}
+
+void	swap(int *a,int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
