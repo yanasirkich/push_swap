7878b7650ff063167b45f08a27deeea20a6ce4b5
Yana Sirkich <yanasplanet@gmail.com>
2024-09-29 17:55:34 +0300

    validation fixed to work with -1

--- srcs/stack/validation.c
+++ srcs/stack/validation.c
@@ -6,24 +6,25 @@
 /*   By: ysirkich <ysirkich@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 16:57:51 by ysirkich          #+#    #+#             */
-/*   Updated: 2024/09/20 17:18:54 by ysirkich         ###   ########.fr       */
+/*   Updated: 2024/09/29 17:54:47 by ysirkich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
+#include <limits.h>
 
 static int	if_unique(t_stack *node, int number);
-static int	push_swap_atoi(char *str);
+static long long	push_swap_atoi(char *str);
 
 int	if_valid(char *str, t_stack *stack_a)
 {
-	int	number;
+	long long	number;
 
 	if (!str || str[0] == '\0')
 		return (-1);
 	number = push_swap_atoi(str);
-	if (number == -1)
+	if (number == LLONG_MAX)
 		return (-1);
 	if (if_unique(stack_a, number) == -1)
 		return (-1);
@@ -41,7 +42,7 @@ static int	if_unique(t_stack *node, int number)
 	return (0);
 }
 
-static int	push_swap_atoi(char *str)
+static long long	push_swap_atoi(char *str)
 {
 	long long	number;
 	int			sign;
@@ -53,7 +54,7 @@ static int	push_swap_atoi(char *str)
 	if (str[index] == '-' || str[index] == '+')
 	{
 		if (!ft_isdigit(str[index + 1]))
-			return (-1);
+			return (LLONG_MAX);
 		if (str[index] == '-')
 			sign = -1;
 		index++;
@@ -61,13 +62,13 @@ static int	push_swap_atoi(char *str)
 	while (str[index])
 	{
 		if (!ft_isdigit(str[index]))
-			return (-1);
+			return (LLONG_MAX);
 		number = number * 10 + (str[index] - '0');
 		if ((number * sign) > INT_MAX || (number * sign) < INT_MIN)
-			return (-1);
+			return (LLONG_MAX);
 		index++;
 	}
 	if (number == 0 && str[0] == '-')
-		return (-1);
+		return (LLONG_MAX);
 	return ((int)(number * sign));
 }
